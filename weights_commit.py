23c71e667ba66aa4ddf68d25efe67bfb84de2515
mdvillagra <mdvillagra@gmail.com>
2024-12-27 13:48:43 -0300

    transform float32 to uint64

--- weights_commit.py
+++ weights_commit.py
@@ -1,4 +1,6 @@
 import json
+import struct
+import numpy as np
 
 def flatten_list(nested_list):
     if not isinstance(nested_list, list):
@@ -9,6 +11,19 @@ def flatten_list(nested_list):
         return flatten_list(nested_list[0])
     return nested_list
 
+def float32_to_uint64(activations):
+    # Convert float32 activations to uint64 encoding
+    float32_array = np.array(activations, dtype=np.float32)
+    uint64_encoded = []
+    
+    for float_val in float32_array:
+        # Pack float32 into bytes then interpret as uint64
+        bytes_val = struct.pack('f', float_val)
+        uint64_val = struct.unpack('Q', bytes_val + b'\x00'*4)[0]  # Pad to 8 bytes
+        uint64_encoded.append(uint64_val)
+        
+    return uint64_encoded
+
 if __name__ == "__main__":
     
     activations_path = "intermediate_activations/base_model_model_lm_head.json"
@@ -17,4 +32,8 @@ if __name__ == "__main__":
 
     activations = flatten_list(activations["input_data"])
 
-    print(len(activations))
+    uint64_activations = float32_to_uint64(activations)
+
+    
+    
+    
