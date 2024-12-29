78bf59219081f8117788e5bb24b1a5b6d82e91e9
mdvillagra <mdvillagra@gmail.com>
2024-12-28 23:46:07 -0300

    testing in python

--- activations_commit.py
+++ activations_commit.py
@@ -1,3 +1,13 @@
 import merkle
 print(merkle.add(3, 5)) 
 print(merkle.minus(3, 5)) 
+
+import random
+
+# Create a list of 10 random floating point numbers
+random_numbers = [random.uniform(-100, 100) for _ in range(10)]
+print("Random numbers:", random_numbers)
+
+# Get the Merkle root hash of these numbers
+merkle_root = merkle.insert_values(random_numbers)
+print("Merkle root:", merkle_root)
