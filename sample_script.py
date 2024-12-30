57ee5aa0ea7d420c970664c3732ef954d79bb0af
mdvillagra <mdvillagra@gmail.com>
2024-12-30 14:40:00 -0300

    moved verify_proof

--- sample_script.py
+++ sample_script.py
@@ -1,13 +1,16 @@
-from zklora import export_lora_submodules, generate_proofs_async
+from zklora import export_lora_submodules_flattened, generate_proofs_async, verify_proof
 
 import asyncio
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
 
+
 def main():
     # 1) Load base & LoRA model
     base_model = AutoModelForCausalLM.from_pretrained("distilgpt2")
-    lora_model = PeftModel.from_pretrained(base_model, "q1e123/peft-starcoder-lora-a100")
+    lora_model = PeftModel.from_pretrained(
+        base_model, "q1e123/peft-starcoder-lora-a100"
+    )
     lora_model.eval()
 
     tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
@@ -24,6 +27,10 @@ def main():
         output_dir="lora_onnx_params",
         json_dir="intermediate_activations",
         submodule_key="attn.c_attn"
+        input_text="Hello from LoRA",
+        output_dir="lora_onnx_params",  # ONNX output
+        json_dir="intermediate_activations",  # JSON output
+        submodule_key="attn.c_attn",  # filter for submodules named 'attn.c_attn'
     )
 
     # 3) Generate proofs for each ONNX+JSON pair
@@ -34,9 +41,18 @@ def main():
         generate_proofs_async(
             onnx_dir="lora_onnx_params",
             json_dir="intermediate_activations",
-            output_dir="proof_artifacts"
+            output_dir="proof_artifacts",
         )
     )
 
+    # verify_proof(proof_file, settings_file, vk_file, srs_file)
+
+
 if __name__ == "__main__":
-    main()
+    #main()
+    verify_proof(
+        "proof_artifacts/base_model_model_transformer_h_0_attn_c_attn.pf",
+        "proof_artifacts/base_model_model_transformer_h_0_attn_c_attn_settings.json",
+        "proof_artifacts/base_model_model_transformer_h_0_attn_c_attn.vk",
+        "proof_artifacts/kzg.srs",
+    )
