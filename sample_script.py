5dce38e738a3e9b4045eb476f98f798589447325
Marcos Villagra <71292159+mdvillagra@users.noreply.github.com>
2025-01-09 14:04:26 -0300

    Type casting in hook and test (#21)
    * type casting in hook
    
    * tested Llama-3.1-8B-Instruct
    
    * tested mixtral 8x7b
    
    * added more loras
    
    * added gpt2
    
    * added distillgpt2 again

--- sample_script.py
+++ sample_script.py
@@ -8,8 +8,8 @@ import os
 import csv
 
 
-base_model_name = "meta-llama/Llama-3.2-1B"
-lora_model_name = "Chryslerx10/Llama-3.2-1B-finetuned-generalQA-peft-4bit"
+base_model_name = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+lora_model_name = "ruslanmv/Medical-Mixtral-7B-v1.5k"
 base_model = AutoModelForCausalLM.from_pretrained(base_model_name)
 lora_model = PeftModel.from_pretrained(base_model, lora_model_name)
 lora_model.eval()
@@ -26,6 +26,7 @@ export_lora_submodules(
     tokenizer=tokenizer,
     input_texts=texts,  # pass list of strings
     verbose=True,
+    submodule_key="k_proj",
 )
 
 csv_path = "proof_metrics.csv"
