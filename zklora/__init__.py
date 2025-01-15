d4cc8f8bf5ea7a22def013dca3d169d77a68c6e5
ppotash <pjpotash@gmail.com>
2025-01-14 22:12:57 -0300

    initial stage 3 + proofs (still times out after proof gen)

--- zklora/__init__.py
+++ zklora/__init__.py
@@ -1,10 +1,18 @@
 from .lora_onnx_exporter import export_lora_submodules
 from .zk_proof_generator import generate_proofs, batch_verify_proofs, ProofPaths, resolve_proof_paths
+from .mpi_lora_onnx_exporter import (
+    normalize_lora_matrices_mpi,
+    LoraShapeTransformerMPI,
+    export_lora_onnx_json_mpi,
+)
 
 __all__ = [
     'export_lora_submodules',
     'generate_proofs',
     'batch_verify_proofs',
     'ProofPaths',
-    'resolve_proof_paths'
+    'resolve_proof_paths',
+    'normalize_lora_matrices_mpi',
+    'LoraShapeTransformerMPI',
+    'export_lora_onnx_json_mpi',
 ]
